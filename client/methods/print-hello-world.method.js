68aad116a4cc208afac556bedc19b9077736aea9
Shrawan Lakhe <shrawanlakhey@gmail.com>
2019-04-16 18:14:19 +0545

    Initial commit

--- client/methods/print-hello-world.method.js
+++ client/methods/print-hello-world.method.js
@@ -3,6 +3,7 @@
 const grpcClient = require('../helpers/grpc-client.helper');
 const client = grpcClient.init("0.0.0.0", 50051, "hello.proto", "HelloWorldRpc")
 
+console.log('client', client);
 client.printHelloWorld({}, (err, response) => {
     if (err) {
         console.error('Error while logging in => ', err.stack);
